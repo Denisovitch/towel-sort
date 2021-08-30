6552ac1257f6b13d8d7135b7785ad6d55694ac0c
Denisovitch <denisglazin@gmail.com>
2021-08-30 16:07:10 +0300

    init- task towels sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
+module.exports = function towelSort(matrix) {
+    if (!Array.isArray(matrix) || matrix.length == 0) return []; //проверим что массив и что он не пустой
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+    let new_arr = [];
+    new_arr = matrix.reduce((acc, cur, i) => {
+        cur.sort((a, b) => (!(i & 1) ? a - b : b - a)).map((e) => acc.push(e));
+        return acc;
+    }, []);
+
+    return new_arr;
+};
+
+// const matrix = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ];
+
+// console.log(towelSort(matrix));
